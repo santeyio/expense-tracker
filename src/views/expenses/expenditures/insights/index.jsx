c8f823d0ff6d627697e38320c9dfe965e4d0ba59
Caleb Hayashida <santeyio@gmail.com>
2024-06-01 23:49:56 -0400

    insights page beneficiary filter buttons work

--- src/views/expenses/expenditures/insights/index.jsx
+++ src/views/expenses/expenditures/insights/index.jsx
@@ -7,7 +7,7 @@ import { filterExpenses } from '../../../../utils/expenses';
 import { toCurrency } from '../../../../utils/prettify';
 
 // components
-import { Table } from '../../../components';
+import { Table, FilterButtons } from '../../../components';
 
 function calculateCategoryTotalsByBeneficiary({
   data,
@@ -35,6 +35,7 @@ function calculateCategoryTotalsByBeneficiary({
 }
 
 function Insights() {
+  const [ selectedBenes, setSelectedBenes ] = useState([]);
   const [ startFilter, setStartFilter ] = useState();
   const [ endFilter, setEndFilter ] = useState();
   const {
@@ -43,7 +44,8 @@ function Insights() {
     db: expenses,
   } = useSelector(store => store.expenses);
 
-  const beneCols = beneficiaries.map(b => ({
+  const beneSubset = beneficiaries.filter(b => selectedBenes.includes(b.id));
+  const beneCols = beneSubset.map(b => ({
     header: b.name,
     field: `bene${b.id}`,
     formatter: (v) => toCurrency(v),
@@ -72,13 +74,27 @@ function Insights() {
   });
 
   return (
-    <div>
-      <Table
-        className="table table-striped"
-        schema={schema}
-        data={summedData}
-      />
-    </div>
+    <>
+      <div className="row mt-4">
+        <h5>Beneficiaries</h5>
+        <FilterButtons
+          filterList={beneficiaries}
+          selectedList={selectedBenes}
+          setSelectedList={setSelectedBenes}
+        />
+      </div>
+
+      <div className="row mt-5">
+        <h5>Category Totals By Beneficiary</h5>
+        <div className="col">
+          <Table
+            className="table table-striped"
+            schema={schema}
+            data={summedData}
+          />
+        </div>
+      </div>
+    </>
   );
 }
 
